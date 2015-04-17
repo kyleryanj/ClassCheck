b10578bfef3e8af398d45903cc89ef662bd89c02
Kyle Ryan <kyleryanj@gmail.com>
2015-04-17 06:48:50 -0400

    33

--- ClassCheck/settings.py
+++ ClassCheck/settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = (
 )
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'checker', 'templates'),
+    os.path.join(BASE_DIR, '/checker/templates/'),
 )
 
 # Simplified static file serving.
